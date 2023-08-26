8d116a2261568630cabe33c76abf60c201c9c4bd
Aidin <aidin.forooghi@gmail.com>
2023-08-26 14:15:53 -0400

    Updating Models

--- lib/db/models.py
+++ lib/db/models.py
@@ -2,3 +2,4 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
