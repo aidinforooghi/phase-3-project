5f7c65ce3cec182949f83bdf80200025e52310b3
Aidin <aidin.forooghi@gmail.com>
2023-08-26 14:18:31 -0400

    Updating Models

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,3 +3,6 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+engine = create_engine("sqlite:///enrollment.db")
