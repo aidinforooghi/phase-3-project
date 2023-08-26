1ff75f367eb874190bff8def44a547d44d3d04a1
Aidin <aidin.forooghi@gmail.com>
2023-08-26 16:12:34 -0400

    Updating cli.py

--- lib/cli.py
+++ lib/cli.py
@@ -8,16 +8,13 @@ def cli():
     pass
 
 
-
 @cli.command()
 def enrollments():
     enrollments = session.query(Enrollment).all()
 
 
-
     for enrollment in enrollments:
         click.echo(f"Student: {enrollment.student.name}, Course: {enrollment.course.name}")
-
 
 
 if __name__ == "__main__":
