e9680b597af4fd74b01c29c6d87d9880191397f0
Aidin <aidin.forooghi@gmail.com>
2023-08-26 15:35:31 -0400

    Updating seed.py

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -3,3 +3,7 @@ from .models import Student, Course, Enrollment, session
 def seed_data():
     student1 = Student(name="John Doe")
     student2 = Student(name="Jane Smith")
+
+
+        course1 = Course(name="Math")
+        course2 = Course(name="Science")
