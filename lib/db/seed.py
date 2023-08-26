5c758cc16f56e23560be74e157243351159e4ebf
Aidin <aidin.forooghi@gmail.com>
2023-08-26 15:39:18 -0400

    Updating seed.py

--- lib/db/seed.py
+++ lib/db/seed.py
@@ -11,3 +11,5 @@ def seed_data():
     enrollment1 = Enrollment(student=student1, course=course1)
     enrollment2 = Enrollment(student=student1, course=course2)
     enrollment3 = Enrollment(student=student2, course=course1)
+
+session.add_all([student1, student2, course1, course2, enrollment1, enrollment2, enrollment3])
