fcac9336e26fea98016cf0320dc2f53434404651
Aidin <aidin.forooghi@gmail.com>
2023-08-26 15:23:51 -0400

    Updating Models

--- lib/db/models.py
+++ lib/db/models.py
@@ -28,3 +28,4 @@ class Enrollment(Base):
     __tablename__ = 'enrollments'
      id = Column(Integer, primary_key=True)
      student_id = Column(Integer, ForeignKey('students.id'))
+     course_id = Column(Integer, ForeignKey('courses.id'))
