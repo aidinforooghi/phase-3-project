48ad701fbc5a43fdba2f5a5a3e7ab3e43385e261
Aidin <aidin.forooghi@gmail.com>
2023-08-26 15:14:01 -0400

    Updating Models

--- lib/db/models.py
+++ lib/db/models.py
@@ -12,3 +12,6 @@ session = Session()
 
 class Student(Base):
     __tablename__ = 'students'
+        id = Column(Integer, primary_key=True)
+    name = Column(String)
+    enrollments = relationship("Enrollment", back_populates="student")
