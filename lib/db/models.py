a80100780b0058a8a358cf567edf5b0f8f1add53
Aidin <aidin.forooghi@gmail.com>
2023-08-26 21:04:15 -0400

    Updating Models & Seed

--- lib/db/models.py
+++ lib/db/models.py
@@ -13,24 +13,25 @@ session = Session()
 
 class Student(Base):
     __tablename__ = 'students'
-        id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     enrollments = relationship("Enrollment", back_populates="student")
 
 
-
 class Course(Base):
     __tablename__ = 'courses'
-        id = Column(Integer, primary_key=True)
+
+    id = Column(Integer, primary_key=True)
     name = Column(String)
     enrollments = relationship("Enrollment", back_populates="course")
 
 
-
 class Enrollment(Base):
     __tablename__ = 'enrollments'
-     id = Column(Integer, primary_key=True)
-     student_id = Column(Integer, ForeignKey('students.id'))
-     course_id = Column(Integer, ForeignKey('courses.id'))
-     student = relationship("Student", back_populates="enrollments")
-     course = relationship("Course", back_populates="enrollments")
+
+    id = Column(Integer, primary_key=True)
+    student_id = Column(Integer, ForeignKey('students.id'))
+    course_id = Column(Integer, ForeignKey('courses.id'))
+    student = relationship("Student", back_populates="enrollments")
+    course = relationship("Course", back_populates="enrollments")
--- lib/db/seed.py
+++ lib/db/seed.py
@@ -1,20 +1,16 @@
 from .models import Student, Course, Enrollment, session
 
+
 def seed_data():
     student1 = Student(name="John Doe")
     student2 = Student(name="Jane Smith")
 
-
-        course1 = Course(name="Math")
-        course2 = Course(name="Science")
-
-
+    course1 = Course(name="Math")
+    course2 = Course(name="Science")
 
     enrollment1 = Enrollment(student=student1, course=course1)
     enrollment2 = Enrollment(student=student1, course=course2)
     enrollment3 = Enrollment(student=student2, course=course1)
 
-
-
-session.add_all([student1, student2, course1, course2, enrollment1, enrollment2, enrollment3])
+    session.add_all([student1, student2, course1, course2, enrollment1, enrollment2, enrollment3])
     session.commit()
